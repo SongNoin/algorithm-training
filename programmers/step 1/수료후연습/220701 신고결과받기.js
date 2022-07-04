8d8ed0e552ca77f111025fe8f9b186a4e4a723d6
SongNoin <as5427072@gmail.com>
2022-07-04 20:46:46 +0900

    알고리즘 풀이 추가

--- programmers/step 1/수료후연습/220701 신고결과받기.js
+++ programmers/step 1/수료후연습/220701 신고결과받기.js
@@ -7,24 +7,25 @@
 //   arr.filter(x => x === "특정 값").length  특정값 갯수
 
 function solution(id_list, report, k) {
-    let realReport = [...new Set(report)];
-    let arr = [];
-    let bann_list = [];
-    let answer = [];
-    for (let j = 0; j < id_list.length; j++) {
-      answer.push(0);
+  let realReport = [...new Set(report)];
+  let arr = [];
+  let bann_list = [];
+  let answer = [];
+  for (let j = 0; j < id_list.length; j++) {
+    answer.push(0);
+  }
+  for (let i = 0; i < realReport.length; i++) {
+    arr.push(realReport[i].split(" ")[1]);
+  }
+  for (let l = 0; l < id_list.length; l++) {
+    if (arr.filter((x) => x === id_list[l]).length >= k)
+      bann_list.push(id_list[l]);
+  }
+  for (let k = 0; k < realReport.length; k++) {
+    if (bann_list.includes(realReport[k].split(" ")[1])) {
+      answer[id_list.indexOf(realReport[k].split(" ")[0])] =
+        answer[id_list.indexOf(realReport[k].split(" ")[0])] + 1;
     }
-    for (let i = 0; i < realReport.length; i++) {
-      arr.push(realReport[i].split(' ')[1]);
-    }
-    for (let l = 0; l < id_list.length; l++) {
-      if (arr.filter(x => x === id_list[l]).length >= k) bann_list.push(id_list[l]);
-    }
-    for (let k = 0; k < realReport.length; k++) {
-      if (bann_list.includes(realReport[k].split(' ')[1])) {
-        answer[id_list.indexOf(realReport[k].split(' ')[0])] =
-          answer[id_list.indexOf(realReport[k].split(' ')[0])] + 1;
-      }
-    }
-    return answer;
+  }
+  return answer;
 }
